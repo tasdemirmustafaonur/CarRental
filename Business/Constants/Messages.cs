177fd2f575d8c85a9345e815c99899d0d16027c5
Mustafa Onur Tasdemir <mustafaonurtasdemirr@gmail.com>
2021-12-23 13:35:50 +0300

    Messages constants updated

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -8,7 +8,9 @@ namespace Business.Constants
 {
     public static class Messages
     {
-        public static string ProductAdded = "Ürün eklendi";
-        public static string ProductNameInvalid = "Ürün ismi geçersiz";
+        public static string CarAdded = "Ürün eklendi";
+        public static string CarNameInvalid = "Ürün ismi geçersiz";
+        public static string MaintenanceTime = "Sistem bakımda";
+        public static string ProductsListed = "Arabalar listelendi";
     }
 }
