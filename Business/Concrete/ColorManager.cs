33564545c15ad163713e11dd6428ee3f7cee30f4
Mustafa Onur Tasdemir <mustafaonurtasdemirr@gmail.com>
2022-01-02 23:18:55 +0300

    ColorManager refactored

--- Business/Concrete/ColorManager.cs
+++ Business/Concrete/ColorManager.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Business.Abstract;
+using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
 
@@ -18,9 +19,29 @@ namespace Business.Concrete
             _colorDal = colorDal;
         }
 
-        public List<Color> GetAll()
+        public IResult Add(Color color)
         {
-            return _colorDal.GetAll();
+            return new SuccessResult(Messages.ColorAdded);
+        }
+
+        public IResult Delete(Color color)
+        {
+            return new SuccessResult(Messages.ColorDeleted);
+        }
+
+        public IDataResult<List<Color>> GetAll()
+        {
+            return new SuccessDataResult<List<Color>>(_colorDal.GetAll(), Messages.ColorsListed);
+        }
+
+        public IDataResult<Color> GetColorById(int id)
+        {
+            return new SuccessDataResult<Color>(_colorDal.Get(c => c.ColorId == id), Messages.ColorListed);
+        }
+
+        public IResult Update(Color color)
+        {
+            return new SuccessResult(Messages.ColorUpdated);
         }
     }
 }
