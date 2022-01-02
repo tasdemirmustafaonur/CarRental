d41854480de8b2cdbb3e0a2b3db7e481b6d6255b
Mustafa Onur Tasdemir <mustafaonurtasdemirr@gmail.com>
2022-01-02 23:14:24 +0300

    IColorService refactored

--- Business/Abstract/IColorService.cs
+++ Business/Abstract/IColorService.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Core.Utilities.Results;
 using Entities.Concrete;
 
 namespace Business.Abstract
 {
     public interface IColorService
     {
-        List<Color> GetAll();
+        IDataResult<Color> GetColorById(int id);
+        IDataResult<List<Color>> GetAll();
+        IResult Add(Color color);
+        IResult Update(Color color);
+        IResult Delete(Color color);
     }
 }
