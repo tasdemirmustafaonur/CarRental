a3e3256d62dcd55f3710dc871cb5bfc8e469ca83
Mustafa Onur Tasdemir <mustafaonurtasdemirr@gmail.com>
2022-01-02 23:06:16 +0300

    IBrandService refactored

--- Business/Abstract/IBrandService.cs
+++ Business/Abstract/IBrandService.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Core.Utilities.Results;
 using Entities.Concrete;
 
 namespace Business.Abstract
 {
     public interface IBrandService
     {
-        List<Brand> GetAll();
+        IDataResult<Brand> GetBrandById(int id);
+        IDataResult<List<Brand>> GetAll();
+        IResult Add(Brand brand);
+        IResult Update(Brand brand);
+        IResult Delete(Brand brand);
     }
 }
