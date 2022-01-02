baf68a30cb60eebb459e0372ff80e75409e50341
Mustafa Onur Tasdemir <mustafaonurtasdemirr@gmail.com>
2022-01-02 23:12:53 +0300

    Messages updated

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -12,5 +12,11 @@ namespace Business.Constants
         public static string CarNameInvalid = "Ürün ismi geçersiz";
         public static string MaintenanceTime = "Sistem bakımda";
         public static string CarsListed = "Arabalar listelendi";
+
+        public static string BrandAdded = "Marka eklendi";
+        public static string BrandDeleted = "Marka silindi";
+        public static string BrandUpdated = "Marka guncellendi";
+        public static string BrandsListed = "Markalar listelendi";
+        public static string BrandListed = "Marka listelendi";
     }
 }
