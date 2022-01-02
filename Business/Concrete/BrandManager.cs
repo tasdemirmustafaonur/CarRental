a524cb6e8be0bb3b8f969fbb29529e20d12150da
Mustafa Onur Tasdemir <mustafaonurtasdemirr@gmail.com>
2022-01-02 23:11:40 +0300

    BrandManager refactored

--- Business/Concrete/BrandManager.cs
+++ Business/Concrete/BrandManager.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Business.Abstract;
+using Business.Constants;
+using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
 
@@ -18,9 +20,29 @@ namespace Business.Concrete
             _brandDal = brandDal;
         }
 
-        public List<Brand> GetAll()
+        public IResult Add(Brand brand)
         {
-            return _brandDal.GetAll();
+            return new SuccessResult(Messages.BrandAdded);
+        }
+
+        public IResult Delete(Brand brand)
+        {
+            return new SuccessResult(Messages.BrandDeleted);
+        }
+
+        public IDataResult<List<Brand>> GetAll()
+        {
+            return new SuccessDataResult<List<Brand>>(_brandDal.GetAll(), Messages.BrandsListed);
+        }
+
+        public IDataResult<Brand> GetBrandById(int id)
+        {
+            return new SuccessDataResult<Brand>(_brandDal.Get(b => b.BrandId == id), Messages.BrandListed);
+        }
+
+        public IResult Update(Brand brand)
+        {
+            return new SuccessResult(Messages.BrandUpdated);
         }
     }
 }
