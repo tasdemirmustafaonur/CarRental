af171fb179e72cac1d09c862f78f8bf63daf9738
Mustafa Onur Tasdemir <mustafaonurtasdemirr@gmail.com>
2022-01-03 20:23:39 +0300

    Added DTO of Rental

---
+++ Entities/DTOs/RentalDetailDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Entities;
+
+namespace Entities.DTOs
+{
+    public class RentalDetailDto:IDto
+    {
+        public int RentalId { get; set; }
+        public decimal DailyPrice { get; set; }
+        public DateTime? RentDate { get; set; }
+        public DateTime? ReturnDate { get; set; }
+    }
+}
