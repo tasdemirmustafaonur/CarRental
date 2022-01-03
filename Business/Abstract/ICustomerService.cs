c7622dd839a34a39bae296e87bdbaf69a4d12be4
Mustafa Onur Tasdemir <mustafaonurtasdemirr@gmail.com>
2022-01-03 23:22:31 +0300

    Added Service of Customer

---
+++ Business/Abstract/ICustomerService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Utilities.Results;
+using Entities.Concrete;
+using Entities.DTOs;
+
+namespace Business.Abstract
+{
+    public interface ICustomerService
+    {
+        IDataResult<List<Customer>> GetAll();
+        IDataResult<List<Customer>> GetCustomerById(int customerId);
+        IDataResult<List<CustomerDetailDto>> GetCustomersDetails();
+        IResult Add(Customer customer);
+        IResult Delete(Customer customer);
+        IResult Update(Customer customer);
+
+    }
+}
