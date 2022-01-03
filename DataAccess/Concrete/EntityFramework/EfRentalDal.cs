a4c4e135293735899a9ec1158e3d713986307316
Mustafa Onur Tasdemir <mustafaonurtasdemirr@gmail.com>
2022-01-03 23:17:02 +0300

    Added DAL Operations of EfRental

---
+++ DataAccess/Concrete/EntityFramework/EfRentalDal.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Entities.DTOs;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfRentalDal:EfEntityRepositoryBase<Rental,CarRentalContext>,IRentalDal
+    {
+        public List<RentalDetailDto> GetRentalsDto()
+        {
+            using (CarRentalContext context = new CarRentalContext())
+            {
+                var result = from r in context.Rentals
+                    join c in context.Cars
+                        on r.CarId equals c.CarId
+                    join cu in context.Customers
+                        on r.CustomerId equals cu.CustomerId
+                    select new RentalDetailDto
+                    {
+                        RentalId = r.RentalId,
+                        DailyPrice = c.DailyPrice,
+                        RentDate = r.RentDate.Value,
+                        ReturnDate = r.ReturnDate.Value
+                    };
+                return result.ToList();
+            }
+        }
+    }
+}
