b35f1eb189a09d7b6d4fd50532454b54d0d8be7a
Mustafa Onur Tasdemir <mustafaonurtasdemirr@gmail.com>
2022-01-03 23:26:30 +0300

    refactored

--- Business/Abstract/ICustomerService.cs
+++ Business/Abstract/ICustomerService.cs
@@ -12,7 +12,7 @@ namespace Business.Abstract
     public interface ICustomerService
     {
         IDataResult<List<Customer>> GetAll();
-        IDataResult<List<Customer>> GetCustomerById(int customerId);
+        IDataResult<Customer> GetCustomerById(int customerId);
         IDataResult<List<CustomerDetailDto>> GetCustomersDetails();
         IResult Add(Customer customer);
         IResult Delete(Customer customer);
