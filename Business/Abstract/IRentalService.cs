1af67e514c7576db202c633d4be04c40bc125c94
Mustafa Onur Tasdemir <mustafaonurtasdemirr@gmail.com>
2022-01-03 23:26:05 +0300

    Added Service of Rental

---
+++ Business/Abstract/IRentalService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Utilities.Results;
+using Entities.Concrete;
+
+namespace Business.Abstract
+{
+    public interface IRentalService
+    {
+        IDataResult<List<Rental>> GetAll();
+        IDataResult<Rental> GetRentalById(int rentalId);
+        IDataResult<List<Rental>> GetCanBeRented();
+        IDataResult<List<Rental>> GetRentalsDetails();
+        IResult Add(Rental rental);
+        IResult Delete(Rental rental);
+        IResult Update(Rental rental);
+    }
+}
