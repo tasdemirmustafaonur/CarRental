0b21327f20033538444e8a7f0f6b20bc8019b11c
Mustafa Onur Tasdemir <mustafaonurtasdemirr@gmail.com>
2022-01-11 01:02:04 +0300

    Added Color Validation Rules

---
+++ Business/ValidationRules/FluentValidation/ColorValidator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities.Concrete;
+using FluentValidation;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class ColorValidator:AbstractValidator<Color>
+    {
+        public ColorValidator()
+        {
+            RuleFor(c => c.ColorName).NotEmpty();
+            RuleFor(c => c.ColorName).NotNull();
+            RuleFor(c => c.ColorName).MinimumLength(2);
+            RuleFor(c => c.ColorName).MaximumLength(10);
+        }
+    }
+}
