350ed75b002338bc0ef7955925d535852c15b230
Mustafa Onur Tasdemir <mustafaonurtasdemirr@gmail.com>
2022-01-11 01:10:08 +0300

    Added Brand Validation Rules

---
+++ Business/ValidationRules/FluentValidation/BrandValidator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities.Concrete;
+using FluentValidation;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class BrandValidator:AbstractValidator<Brand>
+    {
+        public BrandValidator()
+        {
+            RuleFor(b => b.BrandName).NotEmpty();
+            RuleFor(b => b.BrandName).NotNull();
+            RuleFor(b => b.BrandName).MinimumLength(2);
+            RuleFor(b => b.BrandName).MaximumLength(10);
+        }
+    }
+}
