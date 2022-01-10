af4b2520b111d7afe6fbf5deb34a81c947bd3bad
Mustafa Onur Tasdemir <mustafaonurtasdemirr@gmail.com>
2022-01-10 17:45:59 +0300

    Created CarValidator

---
+++ Business/ValidationRules/FluentValidation/CarValidator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities.Concrete;
+using FluentValidation;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class CarValidator : AbstractValidator<Car>
+    {
+        public CarValidator()
+        {
+            RuleFor(c => c.ModelName).NotEmpty();
+            RuleFor(c => c.ModelName).MinimumLength(2);
+            RuleFor(c => c.DailyPrice).NotEmpty();
+            RuleFor(c => c.DailyPrice).GreaterThan(0);
+            //RuleFor(c => c.DailyPrice).GreaterThanOrEqualTo(10).When(c => c.BrandId == 1);
+            RuleFor(c => c.ModelName).Must(StartWithA);
+        }
+
+        private bool StartWithA(string arg)
+        {
+            return arg.StartsWith("A");
+        }
+    }
+}
