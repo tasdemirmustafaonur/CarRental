c65708c75882538c8a327a10a7aaa1516050e6d4
Mustafa Onur Tasdemir <mustafaonurtasdemirr@gmail.com>
2022-01-11 21:45:04 +0300

    Fixed Business Rules to Color

--- Business/Concrete/ColorManager.cs
+++ Business/Concrete/ColorManager.cs
@@ -7,6 +7,7 @@ using Business.Abstract;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
 using Core.Aspects.Autofac.Validation;
+using Core.Utilities.Business;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
@@ -25,12 +26,23 @@ namespace Business.Concrete
         [ValidationAspect(typeof(ColorValidator))]
         public IResult Add(Color color)
         {
+            IResult rulesResult = BusinessRules.Run(CheckIfColorNameExist(color.ColorName));
+            if (rulesResult!=null)
+            {
+                return rulesResult;
+            }
+            _colorDal.Add(color);
             return new SuccessResult(Messages.ColorAdded);
         }
 
         [ValidationAspect(typeof(ColorValidator))]
         public IResult Delete(int colorId)
         {
+            IResult rulesResult = BusinessRules.Run(CheckIfColorIdExist(colorId));
+            if (rulesResult!=null)
+            {
+                return rulesResult;
+            }
             var deletedColor = _colorDal.Get(c => c.ColorId == colorId);
             _colorDal.Delete(deletedColor);
             return new SuccessResult(Messages.ColorDeleted);
@@ -49,7 +61,33 @@ namespace Business.Concrete
         [ValidationAspect(typeof(ColorValidator))]
         public IResult Update(Color color)
         {
+            IResult rulesResult = BusinessRules.Run(CheckIfColorIdExist(color.ColorId), CheckIfColorNameExist(color.ColorName));
+            if (rulesResult!=null)
+            {
+                return rulesResult;
+            }
+            _colorDal.Update(color);
             return new SuccessResult(Messages.ColorUpdated);
+        }
+
+        private IResult CheckIfColorIdExist(int colorId)
+        {
+            var result = _colorDal.GetAll(c => c.ColorId == colorId).Any();
+            if (!result)
+            {
+                return new ErrorResult(Messages.ColorNotExist);
+            }
+            return new SuccessResult();
+        }
+
+        private IResult CheckIfColorNameExist(string colorName)
+        {
+            var result = _colorDal.GetAll(c => c.ColorName == colorName).Any();
+            if (result)
+            {
+                return new ErrorResult(Messages.ColorNameExist);
+            }
+            return new SuccessResult();
         }
     }
 }
