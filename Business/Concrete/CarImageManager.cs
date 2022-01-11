80c2be0e440575acd2f350fceb760a3d1ba598b1
Mustafa Onur Tasdemir <mustafaonurtasdemirr@gmail.com>
2022-01-11 16:54:04 +0300

    Added Manager for Car Image

---
+++ Business/Concrete/CarImageManager.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Abstract;
+using Business.Constants;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+
+namespace Business.Concrete
+{
+    public class CarImageManager : ICarImageService
+    {
+        private ICarImageDal _carImageDal;
+
+        public CarImageManager(ICarImageDal carImageDal)
+        {
+            _carImageDal = carImageDal;
+        }
+
+        public IDataResult<List<CarImage>> GetAll()
+        {
+            return new SuccessDataResult<List<CarImage>>(_carImageDal.GetAll(), Messages.CarsImagesListed);
+        }
+
+        public IDataResult<List<CarImage>> GetCarImages(int carId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataResult<CarImage> GetById(int imageId)
+        {
+            return new SuccessDataResult<CarImage>(_carImageDal.Get(c => c.CarImageId == imageId),
+                Messages.CarImageListed);
+        }
+
+        public IResult Add(CarImage carImage, IFormFile file)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResult Update(CarImage carImage, IFormFile file)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResult Delete(int imageId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
